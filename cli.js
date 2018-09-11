9bdcf735f2784f55a6fa20e30c9ea0d4e13cb31e
AlejandraAAO <aventurero_7_20@hotmail.com>
2018-09-11 18:30:21 -0500

    Veo lo que hay en mi archivo md close #5

--- cli.js
+++ cli.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
-
-//const mdLinks = require("./md-links.js");
+//require al index.js 
+const mdLinks = require("./index.js");
 
 //todos despues del segundo
 const [,, ...args] = process.argv;
-let ruta = args[0];
-
-console.log(`holiiii ${args}`);
+let ruta = args[0];//guardo en una variable el input despues del comando md-links
+let  name = mdLinks.arch(ruta)
+  .then(name => console.log(`holiiii ${name}`))
+  .catch(error => console(error));
+//console.log(`holiiii ${name}`);
--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 //archivo con la logica
 const path = require("path");
+const fs = require("fs");
+
+let pullArch = (ruta)=>{
+//   let ext = path.extname(ruta)
+//   if (ext === ".md"){
+//     console.log("es md muy bien")  
+//   } else {
+//     console.log("nounou")
+//   };
+//  return ext
+return new Promise((resolve, reject) => {
+  fs.readFile(ruta, "utf-8", (error, data) => {
+    if(error) {reject(error)};
+    resolve(data);
+  });
+});
+}
+
+exports.arch = pullArch;
 
