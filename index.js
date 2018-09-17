e6bea1a9bb2924629cff7d8853bdcd4737c53c8c
AlejandraAAO <aventurero_7_20@hotmail.com>
2018-09-17 16:52:11 -0500

    close #10, close #9

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const fs = require("fs");
 
 const mdLinks = (ruta)=>{
   return new Promise((resolve, reject)=>{
-    console.log(ruta);
+    //console.log(ruta);
    resolve (reconocimiento(ruta))
   })
 }
+
+
 
 let pullArch = (ruta)=>{
   let ext = path.extname(ruta)
@@ -15,11 +17,25 @@ let pullArch = (ruta)=>{
     return new Promise((resolve, reject) => {
       fs.readFile(ruta, "utf-8", (error, data) => {
         //console.log(typeof data);
-        const search = data.match(/\[([^\]]+)\](\([^\)]+\)|\[[^\]]+\])/gm)
+        const search = data.match(/\[(.+)\]\((.+)\)/gm);
         //search === array
-        
+        let nombreLink =[];
+        let urlLink = [];
+        let cadalink = search.forEach(etiqueta => {
+          //let url = etiqueta.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gm);
+          //links.push(url); 
+          let signo = etiqueta.indexOf("]");
+          let signo2 = etiqueta.indexOf("(");
+          let marca =  etiqueta.substring(1,signo);
+          nombreLink.push(marca);
+          let link = etiqueta.substring((signo2+1), (etiqueta.length-1));
+          urlLink.push(link);
+        })
         if(error) {reject(error)};
-        resolve(search);
+        resolve(urlLink);
+        //const url = search.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gm);
+        
+        
       });
     });
   }
