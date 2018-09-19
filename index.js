b928e9bf3c01bd981a69d7e2c8e1b4e698635f8e
AlejandraAAO <aventurero_7_20@hotmail.com>
2018-09-19 14:36:32 -0500

    me muestra el objeto con las 3 propiedades

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //archivo con la logica
 const path = require("path");
 const fs = require("fs");
+const fetch = require("node-fetch");
 
 const mdLinks = (ruta)=>{
   return new Promise((resolve, reject)=>{
@@ -9,49 +10,63 @@ const mdLinks = (ruta)=>{
   })
 }
 
+let fecheando = (arrayLinks)=>{
+ return new Promise((resolve)=>{
+   
+   let status = arrayLinks.map(arrayLink => {
+     let result  = fetch(arrayLink)
+     .then(data => data.statusText);
+   }) 
+   
+   resolve(status);
+ })
+}
 
-
-let pullArch = (ruta)=>{
+const pullArch = (ruta)=>{
   let ext = path.extname(ruta)
-  if (ext === ".md"){
+  if (ext === '.md'){
     return new Promise((resolve, reject) => {
       fs.readFile(ruta, "utf-8", (error, data) => {
         //console.log(typeof data);
         const search = data.match(/\[(.+)\]\((.+)\)/gm);
         //search === array
-        let nombreLink =[];
-        let urlLink = [];
-        let cadalink = search.forEach(etiqueta => {
-          //let url = etiqueta.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gm);
-          //links.push(url); 
+        // let nombreLink =[]; 
+        // let urlLink = [];
+        let cadalink = search.map(etiqueta => {
           let signo = etiqueta.indexOf("]");
           let signo2 = etiqueta.indexOf("(");
-          let marca =  etiqueta.substring(1,signo);
-          nombreLink.push(marca);
-          let link = etiqueta.substring((signo2+1), (etiqueta.length-1));
-          urlLink.push(link);
-        })
-        if(error) {reject(error)};
-        resolve(urlLink);
+          let text =  etiqueta.substring(1,signo);
+          let href = etiqueta.substring((signo2+1), (etiqueta.length-1));
+          return ({
+            text: text,
+            href: href,
+            file: ruta,
+          })
+          
+          })
+        if(error) reject(error);
+        //let result = fecheando(urlLink);
+       resolve(cadalink);
         //const url = search.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gm);
         
         
       });
     });
   }
-}
+};
 
   
   const reconocimiento = (ruta)=>{
      const stats = fs.lstatSync(ruta)   
      if(stats.isDirectory()){
+       //retornar un array de
        return console.log("hola");
      } else if(stats.isFile()){    
        return pullArch(ruta)
      }
   }
 
-
+m
 //exports.arch = pullArch;
 exports.read = mdLinks;
 
